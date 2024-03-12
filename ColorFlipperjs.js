2764f38c9776ecb88a51c21106402e805db0b8cf
Aman Nindra <amannindra@gmail.com>
2024-03-11 23:11:38 -0700

    pushed

---
+++ ColorFlipperjs.js
@@ -0,0 +1,10 @@
+const changeColorfunc = document.getElementById('changeColorFunction');
+    changeColorfunc.addEventListener('click',changeColor);
+    function changeColor() {
+        let red = Math.floor(Math.random() * 256);
+        let green = Math.floor(Math.random() * 256);
+        let blue = Math.floor(Math.random() * 256);
+        let color = `rgb(${red}, ${green}, ${blue})`; 
+        document.getElementById("colorFlipper").style.backgroundColor = color;
+        console.log(color);
+    }
